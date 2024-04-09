ea4faa7f7913efb06595ad70af5100d2912dd595
Jadhavsau777 <108616952+Jadhavsau777@users.noreply.github.com>
2024-04-09 17:37:55 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,16 +4,17 @@
 void main()
 {
 
-   int i;
+   int i,sum=0;
    clrscr();
+   printf("Enter Any Number\n");
    for(i=1;i<=5;i++)
    {
-   int num,sum=0;
-   printf("Enter Number");
+   int num;
    scanf("%d",&num);
    sum=sum+num;
    }
    printf("The Sum Of All Numbers Entered Is %d",sum);
    
+
    getch();
-}
+}  
